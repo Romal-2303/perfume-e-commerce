aa022fc905b08985b9b80b40f383fd86350ebc36
Arshdeep-0509 <arshdeepsinghkhalsa2001@gmail.com>
2024-12-20 14:54:46 +0530

    checkout page

---
+++ src/pages/Checkout/Checkout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from "./Checkout.module.scss"
+
+const Checkout = () => {
+  return (
+   <div className={classes['checkout-container']}>
+    <div className={classes['left-conatiner']}></div>
+    <div className={classes['right-container']}></div>
+   </div>
+  )
+}
+
+export default Checkout
